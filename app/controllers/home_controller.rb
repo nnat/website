9963d5d3fa512ee06c14cd3512e1adbf20f5304a
Adrien Thery <adrien.thery@gmail.com>
2014-10-08 23:18:56 +0200

    hidden homepage with why content

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
     @lead = Lead.new
   end
+  #hidden homepage
+  def well_not_ready_yet
+  	@lead = Lead.new
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  get 'well_not_ready_yet' => 'home#well_not_ready_yet'
 
   resources :leads
 
