c6acba0b1bfe00c497417e62cac453076b4b6b89
nna <nico.nardone@gmail.com>
2015-02-06 00:18:10 +0100

    Fix IP filtering

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
+  include UserFilteringHelper
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   before_action :restrict_access, :set_locale
 
 private
