29852da523b5fcb1cb95890bbef1d11733376959
Adrien Thery <adrien.thery@gmail.com>
2015-04-29 13:45:51 +0200

    handle payment errors

--- app/controllers/program_leads_controller.rb
+++ app/controllers/program_leads_controller.rb
@@ -23,7 +23,8 @@ class ProgramLeadsController < ApplicationController
       
 
     rescue Stripe::CardError => e
-      flash.now[:alert] = "Désolé la transaction n'a pas pu aboutir. Envoyez-nous un email"
+      @payment_error = e.to_json
+      @payment_error_message = payment_error_message(e)
       render :new
       return
     end
@@ -42,5 +43,25 @@ private
 
   def lead_params
     params.require(:lead).permit(:email, :first_name, :last_name, :post_code)
+  end
+
+  def payment_error_message stripe_error
+    code = stripe_error.json_body[:error][:code]
+    res = case code
+      when 'incorrect_number' then "Le numéro de la carte est incorrect"
+      when 'invalid_number' then "Le numéro de la carte n'est pas un numéro de carte valide"
+      when 'invalid_expiry_month' then "Le mois d'expiration n'est pas valide"
+      when 'invalid_expiry_year' then "L'année d'expiration n'est pas valide"
+      when 'invalid_cvc' then  "Le cryptogramme n'est pas valide"
+      when 'expired_card' then "Cette carte a expirée, vous pouvez essayer de nouveau avec une autre carte"
+      when 'incorrect_cvc' then "Le cryptogramme n'est pas correct"
+      when 'incorrect_zip' then "Le zip code est incorrect"
+      when 'card_declined' then "Cette carte a été refusée, vous pouvez essayer de nouveau avec une autre carte"
+      when 'missing' then "Carte absente"
+      when 'processing_error' then "Une erreur est survenue lors du traitement de la carte. Votre carte n'a pas été débitée, vous pouvez essayer de nouveau"
+      when 'rate_limit' then "Une erreur technique est survenue. Votre carte n'a pas été débitée, vous pouvez essayer de nouveau"
+      else "Une erreur technique est survenue. Votre carte n'a pas été débitée, vous pouve essayer de nouveau"
+    end
+    return res
   end
 end
