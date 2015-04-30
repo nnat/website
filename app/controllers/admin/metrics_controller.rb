a938f055a4b3f69ed74239678422e30b11ca5003
Adrien Thery <adrien.thery@gmail.com>
2015-04-30 10:40:11 +0200

    display paying leads in admin tool

--- app/controllers/admin/metrics_controller.rb
+++ app/controllers/admin/metrics_controller.rb
@@ -1,7 +1,8 @@
 class Admin::MetricsController < Admin::BaseController
   def index
-    @lead_count   = Lead.count
+    @lead_count    = Lead.count
     @nb_last_leads = params[:nb].present? ? params[:nb].to_i : 5
-    @last_leads = Lead.last(@nb_last_leads)
+    @last_leads    = Lead.last(@nb_last_leads)
+    @paying_leads  = Lead.applied_recently
   end
 end
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,4 +2,6 @@ class Lead < ActiveRecord::Base
   validates_presence_of   :email, message: I18n.t('lead.validation.presence')
   validates_format_of     :email, message: I18n.t('lead.validation.format'), with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates_uniqueness_of :email, message: I18n.t('lead.validation.uniqueness')
+
+  scope :applied_recently, ->  { where.not(applied_at: nil).order(applied_at: :desc) }
 end
