38831cd7b6510fdb2fd6045c7ebfa09990124bc1
Nicolas NARDONE <nico.nardone@gmail.com>
2016-05-13 18:54:53 +0200

    new offer page

--- app/controllers/concerns/offers_controller.rb
+++ app/controllers/concerns/offers_controller.rb
@@ -3,6 +3,25 @@ class OffersController < ApplicationController
   def index
     @section = :program
     @chosen_version = params[:version]
+    @offer = {
+    	'ultimate' => {
+    		'location' => {
+    			price: '149€ par mois',
+    			duration: 'Engagement 12 mois, reprise gratuite'
+    		},
+    		'achat' => {
+    			price: '2590€'
+    		}
+    	},
+    	'medium' => {
+    		'location' => {
+    			price: '99€ par mois'
+    		},
+    		'achat' => {
+    			price: '1990€'
+    		}
+    	},
+    }
   end
 
 end
