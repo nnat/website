6becfbdc71ff3a07384cb7dc6ef408c2e44b780a
Nicolas NARDONE <nico.nardone@gmail.com>
2016-08-05 09:27:28 +0200

    fix catch all routes

--- config/routes.rb
+++ config/routes.rb
@@ -80,7 +80,7 @@ end
   # catch all /app and /pastouch without https and redirect to same url using https
   # match "offre(/*path)", constraints: http_catchall, via: [:get], to: redirect { |params, request| "https://" + request.host_with_port + request.fullpath }
   match "pastouch(/*path)", constraints: http_catchall, via: [:get], to: redirect { |params, request| "https://" + request.host_with_port + request.fullpath }
-  match "/*path", constraints: https_catchall, via: [:get], to: redirect { |params, request| "http://" + request.host_with_port + request.fullpath } #Force HTTP in production as there is no more HTTPS
+  match "*path", constraints: https_catchall, via: [:get], to: redirect { |params, request| "http://" + request.host_with_port + request.fullpath } #Force HTTP in production as there is no more HTTPS
 end
 
 
